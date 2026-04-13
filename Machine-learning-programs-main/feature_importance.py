25fb87d49f749c17cc7382b68b9c22be778630f5
sarvaiyameet80-prog <sarvaiyameet80@gmail.com>
2026-04-13 18:08:03 +0530

    Update feature_importance.py

--- Machine-learning-programs-main/feature_importance.py
+++ Machine-learning-programs-main/feature_importance.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_csv(r"D:\Hardik\auto-mpg.csv")
+df = pd.read_csv(r"D:\meet\auto-mpg.csv")
 print(df)
 feat_labels = df.columns[1:]
 
@@ -15,19 +15,19 @@ df.replace('?', np.nan, inplace=True)
 X = df.drop(columns=['mpg', 'car name'])
 y = df['mpg']
 
-# Split the dataset into training and testing sets
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a decision tree classifier
+
 clf = DecisionTreeRegressor(random_state=42)
 
-# Fit the classifier to the training data
+
 clf.fit(X_train, y_train)
 
-# Get feature importances
+
 feature_importances = clf.feature_importances_
 
-# Print feature importances
+
 for feature, importance in zip(df.columns, feature_importances):
     print(f"{feature}: {importance}")
 
