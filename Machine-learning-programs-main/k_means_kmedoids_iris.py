fbdcbd4915fee1cad42f90cb25d3a12c326127ce
sarvaiyameet80-prog <sarvaiyameet80@gmail.com>
2026-04-13 18:10:44 +0530

    Refactor k-means and k-medoids clustering code

--- Machine-learning-programs-main/k_means_kmedoids_iris.py
+++ Machine-learning-programs-main/k_means_kmedoids_iris.py
@@ -6,33 +6,33 @@ from sklearn_extra.cluster import KMedoids
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.metrics import silhouette_score
 
-# Load the Iris dataset
+
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-# Perform k-means clustering
-k = 3  # number of clusters
+
+k = 3  
 kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
 
-# Get cluster labels for k-means
+
 labels = kmeans.labels_
 
-#print silhouette score for kmeans
+
 print("Silhouette score for k-means is: ", (silhouette_score(X, labels, metric='euclidean')))
 
-# Perform k-medoids clustering
-k = 3  # number of clusters
+
+k = 3 
 kmedoids = KMedoids(n_clusters=k, random_state=0).fit(X)
 
-# Get cluster labels for kmedoids
+
 labels = kmedoids.labels_
 
-#print silhouette score for kmedoids
+
 print("Silhouette score for kmedoids is: ", (silhouette_score(X, labels, metric='euclidean')))
 
 
-# Visualize the clusters (using first two features)for k-means
+
 plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='viridis')
 centers = kmeans.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75)
@@ -42,7 +42,6 @@ plt.ylabel('Sepal Width (cm)')
 plt.show()
 
 
-# Visualize the clusters (using first two features) for kmedoids
 plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='viridis')
 plt.title('KMedoids Clustering of Iris Dataset')
 plt.xlabel('Sepal Length (cm)')
