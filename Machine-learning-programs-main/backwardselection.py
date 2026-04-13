b17c38a4e370ff3f6f158ab0c057cee5c4820eae
sarvaiyameet80-prog <sarvaiyameet80@gmail.com>
2026-04-13 18:00:33 +0530

    Update backwardselection.py

--- Machine-learning-programs-main/backwardselection.py
+++ Machine-learning-programs-main/backwardselection.py
@@ -4,37 +4,37 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
-#1. load and clean dataset
 
-df = pd.read_csv(r"D:\Hardik\auto-mpg.csv")
 
-# replace '?' with NAN
+df = pd.read_csv(r"D:\meet\auto-mpg.csv")
+
+
 df.replace("?", np.nan, inplace=True)
 
-# convert horsepower to numeric
+
 df["horsepower"] = pd.to_numeric(df["horsepower"])
 
-# drop missing values
+
 df = df.dropna()
 
-# drop non-numeric column (car name)
+
 if "car name" in df.columns:
     df = df.drop(columns=["car name"])
 
-#2. Define Features and Target
+
 X = df.drop(columns=["mpg"])
 Y = df["mpg"]
 
-# train-test split
+
 X_train, X_test, Y_train, Y_test = train_test_split(
     X, Y, test_size=0.2, random_state=42
 )
 
-#3. Backward feature selection
+
 
 selected_features = list(X.columns)
 
-#Train full model first
+
 model = LinearRegression()
 model.fit(X_train[selected_features], Y_train)
 y_pred = model.predict(X_test[selected_features])
@@ -59,7 +59,7 @@ while len(selected_features) > 1:
 
         scores.append((score, feature))
 
-    #Find removal that gives highest R2
+    
     scores.sort(reverse=True)
     current_best_score, worst_feature = scores[0]
 
